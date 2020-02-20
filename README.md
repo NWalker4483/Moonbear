4a912521c1e30d251fb334f4f1b47779f92b62a8
Master <master@master.remote.com>
2020-02-20 13:31:58 -0500

    Added .gitignore

---
+++ README.md
@@ -0,0 +1,2 @@
+sudo apt-get install ros-kinetic-ar-track-alvar
+sudo apt-get install ros-kinetic-usb-cam
--- src/ursaring/scripts/CameraRotater.py
+++ src/ursaring/scripts/CameraRotater.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 import RPi.GPIO as GPIO
 import time
+
+control_pins = [31,33,35,37]
+'''
 halfstep_seq = [
   [1,0,0,0],
   [1,1,0,0],
@@ -13,27 +17,28 @@ halfstep_seq = [
   [0,0,0,1],
   [1,0,0,1]
 ]
+'''
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-
-for i in range(512):
-  for halfstep in range(8):
-    for pin in range(4):
-      GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
-    time.sleep(0.001)
-
+def callback(msg):
+    	rospy.loginfo()
+	'''
+	for i in range(512):
+		for halfstep in range(8):
+			for pin in range(4):
+				GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+			time.sleep(0.001)
+	'''
 if __name__ == '__main__':
 	GPIO.setmode(GPIO.BOARD)
-	control_pins = [7,11,13,15]
+	control_pins = [31,33,35,37]
 	for pin in control_pins:
 	  GPIO.setup(pin, GPIO.OUT)
 	  GPIO.output(pin, 0)
 	# In ROS, nodes are uniquely named. If two nodes with the same
 	# name are launched, the previous one is kicked off.
 	rospy.init_node('camera_platform')
-
-	rospy.Subscriber("chatter", String, callback)
+	# TODO Change to a /camera_angle topic 
+	rospy.Subscriber("cmd_vel", Twist, callback)
 
 	# spin() simply keeps python from exiting until this node is stopped
 	rospy.spin()
