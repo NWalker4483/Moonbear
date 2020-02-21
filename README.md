d6ff40fefbfe15a2ee841a981f6164d99d52f3be
NWalker4483 <niwal7@morgan.edu>
2020-02-20 20:25:07 -0500

    Tags Tracked but not displayed due to missing tf

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 sudo apt-get install ros-kinetic-ar-track-alvar
 sudo apt-get install ros-kinetic-usb-cam
+sudo apt-get install ros-indigo-compressed-image-transport
+rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.108 image:=/my_camera/image camera:=/my_camera
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -126,6 +126,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/master/catkin_ws/build/gtest/cmake_install.cmake")
+  include("/home/master/catkin_ws/build/ar_tag_toolbox/cmake_install.cmake")
   include("/home/master/catkin_ws/build/ursaring/cmake_install.cmake")
 
 endif()
