8e8c7b9ad8af44af64f73975854eedb07c4a283c
NWalker4483 <niwal7@morgan.edu>
2020-02-27 18:39:24 -0500

    Removed ODOM from arduino

--- src/ursaring/arduino/src/src.ino
+++ src/ursaring/arduino/src/src.ino
@@ -9,9 +9,10 @@
 #define LeftEncoderPinA      3 // Digital Interrupt Pin
 #define LeftEncoderPinB      5// Digital Pin
 #define StatusModePin        8 // Input Pin acting as a jumper
-#define SimpleSerialModePin  11 // Input Pin acting as a jumper
+#define SimpleSerialModePin  11 // Input Pin acting as a jumpera
 /////////////////////////////
-#define RightTreadControlPin 14
+#define UsingMixedMode       true
+#define RightTreadControlPin A0
 #define LeftTreadControlPin  0
 // For Motor drivers with mixed mode 
 #define XPin                 0
@@ -26,64 +27,50 @@
 #define WHEEL_DIAMETER       12 // cm
 #define PI                   3.1415926535897932384626433832795
 /////////////////////////////
+#define LoopTime             100
+/////////////////////////////a
 #include <ros.h>
-#include <nav_msgs/Odometry.h>
-#include <tf/tf.h>
-#include <tf/transform_broadcaster.h>
 #include <ros/time.h>
+
+#include <std_msgs/Int16.h>
 #include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
 
 //Globals
 unsigned long lastMilli = 0; // time at the end of the last loop 
 byte mode = 'R'; // ROS Mode
 byte ledstatus = LOW;
 
-double current_x = 0;
-double current_y = 0;
-double current_yaw = 0;
-bool publish_tf = false;
-
-double delta_x = 0.0;
-double delta_y = 0.0;
-double linear_velocity = 0.0;
-double angular_velocity = 0.0;
-
-int current_throttle_setting = 0;
-int actual_throttle = 0;
-int goal_throttle = 0;
-int current_steering_angle = 0;
-
-int right_rpm = 0;
+int right_rpm= 0;
 volatile unsigned long right_pulses = 0; // rev counter
 unsigned long old_right_pulses = 0;
 
-int left_rpm = 0;
+int left_rpm= 0;
 volatile unsigned long left_pulses = 0; // rev counter
 unsigned long old_left_pulses = 0;
 
-////
-void DriverCallback(const ackermann_msgs::AckermannDriveStamped&);
-////
 ros::NodeHandle nh;
 ros::Time current_time;
-nav_msgs::Odometry odom_msg;
 
+std_msgs::Int16 left_ticks_msg;
+std_msgs::Int16 right_ticks_msg;
+
+ros::Publisher pub_left_ticks("/left_ticks",&left_ticks_msg);
+ros::Publisher pub_right_ticks("/right_ticks",&right_ticks_msg);
+
+void DriverCallback(const geometry_msgs::Twist&);
 ros::Subscriber<geometry_msgs::Twist> drive("cmd_vel", &DriverCallback); 
-ros::Publisher odom_pub("odom", &odom_msg);
-tf::TransformBroadcaster broadcaster;
 
 void getMotorData(unsigned long time) {
   double delta_time = double(time)/1000; // must be in seconds for these formulas 
-  rpm = (60*(double(right_pulses-oldright_pulses)/ENCODER_RESOLUTION))/delta_time; // 60 is to convert from seconds to minutes
-  oldright_pulses = right_pulses;
-  // store your measured speed setting in this variable
-  actual_throttle = map(rpm,0,PERCENT_100_RPM,0,100); // 0 :-: 100
-  actual_throttle = actual_throttle * (rpm/abs(rpm)); // -100 :-: 100
-  //basic velocity inputs
-  linear_velocity = (rpm * (WHEEL_DIAMETER/100.0)*PI)/delta_time; // m/s from rear wheels
-  current_steering_angle; // must be in radians ... I think 
-  //first get the theta update
-  angular_velocity = linear_velocity*(tan(current_steering_angle)/WHEEL_BASE);
+  
+  right_rpm = (60*(double(right_pulses-old_right_pulses)/ENCODER_RESOLUTION))/delta_time; // 60 is to convert from seconds to minutes
+  right_ticks_msg.data += right_pulses - old_right_pulses;
+  old_right_pulses = right_pulses;
+  
+  left_rpm = (60*(double(left_pulses-old_left_pulses)/ENCODER_RESOLUTION))/delta_time; // 60 is to convert from seconds to minutes
+  left_ticks_msg.data += left_pulses - old_left_pulses;
+  old_left_pulses = left_pulses;
 }
 
 void RightMotorEncoder() { // Counts right_pulses on the  Encoder
@@ -119,21 +106,17 @@ void LeftMotorEncoder() { // Counts right_pulses on the  Encoder
 }
 
 
-void set_RightTread(int speed){
+void set_RightTread(int _speed){
 }
-void set_LeftTread(int speed){
+void set_LeftTread(int _speed){
 }
 
 void DriverCallback(const geometry_msgs::Twist& cmd_msg) {
   // Lin -.5:.5  Ang -1.5:1.5
-  double linear = cmd_msg.drive.speed;
-  double steering_radians = cmd_msg.drive.steering_angle;
-
-  int speed = map(linear,-.5,.5,-100,100); 
-  int steering_angle = steering_radians * (180/PI);
-
-  set_Steering(steering_angle);
-  set_Throttle(speed); 
+  if (UsingMixedMode){
+    analogWrite(RightTreadControlPin,mapf(cmd_msg.linear.x,-.5,.5,0,1023)); 
+    analogWrite(LeftTreadControlPin,mapf(cmd_msg.angular.z,-.5,.5,0,1023)); 
+  }
 }
 int parseIntFast(int numberOfDigits){
   /*
@@ -153,83 +136,37 @@ void ReadSerialCommands(){ // Plot PID Values using Serial Plotter
     char cmdByte = Serial.read();
     int setting;
     switch (cmdByte) {
-      case 'T':
+      case 'R':
         setting = parseIntFast(4);
-        set_Throttle(setting); 
-        Serial.print("Set Throttle to ");
-        Serial.println(current_throttle_setting); 
+        set_RightTread(setting);
+        Serial.print("Set Right to ");
+        Serial.println(0); 
         break;
-      case 'S':
+      case 'L':
         setting = parseIntFast(4);
-        set_Steering(setting); 
-        Serial.print("Set Steering to ");
-        Serial.println(current_steering_angle); 
+        set_LeftTread(setting); 
+        Serial.print("Set Left to ");
+        Serial.println(0); 
         break;
     }
   Serial.flush();
   }
 }
-void PublishODOM(unsigned long time) {
-  double delta_time = double(time)/1000; // must be in seconds
-  //compute odometry update values
-  delta_x = linear_velocity*cos(current_yaw);
-  delta_y = linear_velocity*sin(current_yaw);
-  //Update the pose estimate
-  current_x += delta_x * delta_time;
-  current_y += delta_y * delta_time;
-  current_yaw += angular_velocity * delta_time;
-  if (publish_tf){PublishTransform();}
-
-  odom_msg.header.stamp = current_time; // nh.now();
-  odom_msg.header.frame_id = "/odom";
-  odom_msg.child_frame_id = "/base_link";
-  odom_msg.pose.pose.position.x = current_x;
-  odom_msg.pose.pose.position.y = current_y;
-  odom_msg.pose.pose.position.z = 0.0;
-
-  odom_msg.pose.pose.orientation.x = 0.0;
-  odom_msg.pose.pose.orientation.y = 0.0;
-  odom_msg.pose.pose.orientation.z = sin(current_yaw/2.0);
-  odom_msg.pose.pose.orientation.w = cos(current_yaw/2.0);
+void PublishTICKS(unsigned long time) {
+  pub_left_ticks.publish(&left_ticks_msg);
+  pub_right_ticks.publish(&right_ticks_msg);
   
-  odom_msg.twist.twist.linear.x = linear_velocity;
-  odom_msg.twist.twist.linear.y = 0.0;
-  odom_msg.twist.twist.angular.z = angular_velocity;
-
-  // Position uncertainty
-  odom_msg.pose.covariance[0]  = 0.2; // x
-  odom_msg.pose.covariance[7]  = 0.2; // y
-  odom_msg.pose.covariance[35] = 0.4; // yaw
-
-  odom_pub.publish(&odom_msg);
-  nh.spinOnce();
-}
-void PublishTransform(){
-  geometry_msgs::TransformStamped t;
-  
-  t.header.frame_id = "/odom";
-  t.child_frame_id = "/base_link";
-  t.transform.translation.x = current_x;
-  t.transform.translation.y = current_y;
-  t.transform.translation.z = 0.0;
-  
-  t.transform.rotation.x = 0.0;
-  t.transform.rotation.y = 0.0;
-  t.transform.rotation.z = sin(current_yaw/2.0);
-  t.transform.rotation.w = cos(current_yaw/2.0);
-  
-  t.header.stamp = current_time;
-
-  broadcaster.sendTransform(t);
+  left_ticks_msg.data = 0;
+  right_ticks_msg.data = 0;
 }
 
 void SendStatusInfo() {// TODO: Printing all this data causes a large delay in the speed updater 
   Serial.print("THROTTLE SETTING: ");
-  Serial.println(current_throttle_setting);
+  Serial.println(0);
   Serial.print("STEERING SETTING: ");
-  Serial.println(current_steering_angle);
+  Serial.println(0);
   Serial.print("RPM: ");
-  Serial.println(rpm);
+  Serial.println(0);
   /*
   Serial.print("LINEAR VELOCITY: ");
   Serial.print(linear_velocity);
@@ -240,7 +177,7 @@ void SendStatusInfo() {// TODO: Printing all this data causes a large delay in t
   Serial.print("right_pulses SINCE LAST UPDATE: ");
   Serial.println(right_pulses);*/
   Serial.print("TOTAL right_pulses: ");
-  Serial.println(abs(oldright_pulses));  
+  Serial.println(abs(old_right_pulses));  
   Serial.print("\n");  
 }
 
@@ -253,7 +190,11 @@ void Blink(int times){
     times-=1; 
   }
 }
-  
+
+double mapf(double val, double in_min, double in_max, double out_min, double out_max) {
+    return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 void CheckModeJumpers(){
   pinMode(StatusModePin, INPUT);
   pinMode(SimpleSerialModePin, INPUT);
@@ -272,24 +213,32 @@ void CheckModeJumpers(){
     nh.initNode();
     nh.getHardware()->setBaud(57600); // Ros Node uses 57600 by default
     nh.subscribe(drive);
-    nh.advertise(odom_pub);
+    nh.advertise(pub_left_ticks);
+    nh.advertise(pub_right_ticks);
+    left_ticks_msg.data = 0;
+    right_ticks_msg.data = 0;
     mode = 'R';
   }
 }
 
 void setup() {
-  CheckModeJumpers();
+  CheckModeJumpers(); // Read the jumpers to determine settings
 
+  pinMode(RightTreadControlPin, OUTPUT); 
   pinMode(RightEncoderPinA, INPUT); 
   pinMode(RightEncoderPinB, INPUT); 
-  attachInterrupt(digitalPinToInterrupt(RightEncoderPinA), RightMotorEncoder, CHANGE); // Trigger RPM counter whenever hall sensor right_pulses
+  // digitalPinToInterrupt(RightEncoderPinA) == 0
+  attachInterrupt(0, RightMotorEncoder, CHANGE); // Trigger right_rpmcounter whenever hall sensor pulses
   
+  pinMode(LeftTreadControlPin, OUTPUT);
   pinMode(LeftEncoderPinA, INPUT); 
   pinMode(LeftEncoderPinB, INPUT); 
-  attachInterrupt(digitalPinToInterrupt(LeftEncoderPinA),  LeftMotorEncoder, CHANGE); // Trigger RPM counter whenever hall sensor right_pulses
+  // digitalPinToInterrupt(LeftEncoderPinA) == 1
+  attachInterrupt(1,  LeftMotorEncoder, CHANGE); // Trigger right_rpmcounter whenever hall sensor pulses
 }
 
 void loop() {
+  mode = 'R';
   if(mode=='R'){nh.spinOnce();}
   unsigned long time = millis(); // time - lastMilli == time passed
   //OUPUTS
@@ -299,7 +248,7 @@ void loop() {
       case 'S': // Send Status Info
         SendStatusInfo(); break;
       case 'R': // ROS Mode
-        PublishODOM(time - lastMilli); break; // Publish and Restart Loop 
+        PublishTICKS(time - lastMilli); break; // Publish and Restart Loop 
     }
     lastMilli = time;
   }  //INPUTS 
@@ -308,4 +257,5 @@ void loop() {
       ReadSerialCommands(); break;
   }
 }
+//https://answers.ros.org/question/73627/how-to-increase-rosserial-buffer-size/
 ///http://andrewjkramer.net/motor-encoders-arduino/
