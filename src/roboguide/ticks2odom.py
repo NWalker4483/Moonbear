942d9663527436e797073d3e0d1afbfab3612f91
walkenz1 <nile.walker@jhuapl.edu>
2020-03-02 12:06:13 -0500

    Updated odom

--- src/roboguide/ticks2odom.py
+++ src/roboguide/ticks2odom.py
@@ -3,7 +3,7 @@ import rospy, math
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Int16
 from ackermann_msgs.msg import AckermannDriveStamped
-
+from numpy import sin, cos
 def convert_trans_rot_vel_to_steering_angle(v, omega, wheelbase):
   if omega == 0 or v == 0:
     return 0
@@ -12,32 +12,44 @@ def convert_trans_rot_vel_to_steering_angle(v, omega, wheelbase):
   return math.atan(wheelbase / radius)
 
 def tck_callback(data):
-	delta_time = rospy.Time.now() - last_msg // seconds
-"""
-	#basic velocity inputs
-	propel = 0.0; //m/s from rear wheels
-	steer_angle = a; //radians
-	delta_t = 0.03; //seconds
+	global wheelbase
+	global steering_angle
+	global last_msg
+	global pub
+	delta_time = rospy.Time.now() - last_msg # seconds
+	speed = a * motor_value + b
+steering_angle = a * motor_value + b
+	angular_velocity = speed * tan(steering_angle) / wheelbase_ # rad or degree 
+	ros::Duration dt = time_duration
 
-	#compute odometry values from joint angles
-	#first get the theta update
-	delta_th = (propel/L)*std::tan(steer_angle);
+	double x_dot = current_speed * cos(yaw_)
+	double y_dot = current_speed * sin(yaw_)
+	x_ += x_dot * dt.toSec()
+	y_ += y_dot * dt.toSec()
 
-	#compute odometry update values
-	delta_x = propel*std::cos(position_th + ((delta_th*delta_t)/2.0));
-	delta_y = propel*std::sin(position_th + ((delta_th*delta_t)/2.0));
+	yaw_ += current_angular_velocity * dt.toSec()
 
-	#now update our pose estimate
-	position_x = position_x + delta_x * delta_t;
-	position_y = position_y + delta_y * delta_t;
-	position_th = position_th + delta_th * delta_t;
 
-	odomMsg.pose.pose.position.x = vx
-	odomMsg.pose.pose.position.y = vy
-	odomMsg.pose.pose.orientation.z = v_th
+	odom.pose.pose.position.x = x_
+	odom.pose.pose.position.y = y_
+	odom.pose.pose.orientation.x = 0.0
+	odom.pose.pose.orientation.y = 0.0
+	odom.pose.pose.orientation.z = sin(yaw_/2.0)
+	odom.pose.pose.orientation.w = cos(yaw_/2.0)
+
+	# Co variance should be cacluated empirically
+	odom.pose.covariance[0]  = 0.2 ///< x
+	odom.pose.covariance[7]  = 0.2 ///< y
+	odom.pose.covariance[35] = 0.4 ///< yaw
+
+	# Velocity
+	odom.twist.twist.linear.x = current_speed
+	odom.twist.twist.linear.y = 0.0
+	odom.twist.twist.angular.z = current_angular_velocity	
+	
 	odomMsg.header.frame_id = 'odom'
 	odomMsg.child_frame_id = 'base_footprint'
-"""
+
 	wheelBase=1.68
 	speed= (vR + vL) / 2.0 # m/s 
 	v_th=speed*tan(wheelAngle)/ wheelBase
@@ -49,30 +61,24 @@ def tck_callback(data):
 	odomMsg.header.frame_id = 'odom'
 	odomMsg.child_frame_id = 'base_footprint'
 	pub.publish(odomMsg)
+	last_msg = 0
 
 def cmd_callback(data):
   global wheelbase
-  global ackermann_cmd_topic
-  global frame_id
+  global steering_angle
+  global last_msg
   global pub
   
   v = data.linear.x
   steering = convert_trans_rot_vel_to_steering_angle(v, data.angular.z, wheelbase)
   
-  msg = AckermannDriveStamped()
-  msg.header.stamp = rospy.Time.now()
-  msg.header.frame_id = frame_id
-  msg.drive.steering_angle = steering
-  msg.drive.speed = v
-  
   pub.publish(msg)
   
-
 if __name__ == '__main__': 
-  try:
+  try:    
     
-    rospy.init_node('cmd_vel_to_ackermann_drive')
-        
+	rospy.init_node('cmd_vel_to_ackermann_drive')
+
     twist_cmd_topic = rospy.get_param('~twist_cmd_topic', '/cmd_vel') 
     ackermann_cmd_topic = rospy.get_param('~ackermann_cmd_topic', '/ackermann_cmd')
     wheelbase = rospy.get_param('~wheelbase', 10.0)
@@ -81,9 +87,7 @@ if __name__ == '__main__':
     rospy.Subscriber("/ackermann_cmd", Twist, cmd_callback, queue_size=1)
     rospy.Subscriber("/ticks", Int16, tck_callback, queue_size=1)
     pub = rospy.Publisher(ackermann_cmd_topic, AckermannDriveStamped, queue_size=1)
-    
-    rospy.loginfo("Node 'cmd_vel_to_ackermann_drive' started.\nListening to %s, publishing to %s. Frame id: %s, wheelbase: %f", "/cmd_vel", ackermann_cmd_topic, frame_id, wheelbase)
-    
+
     rospy.spin()
     
   except rospy.ROSInterruptException:
