9a5ca70ebe603cfc17773464c1c755a7d35aab75
walkenz1 <nile.walker@jhuapl.edu>
2020-03-03 18:10:45 -0500

    Added State Publishing

--- src/roboguide/arduino/src/src.ino
+++ src/roboguide/arduino/src/src.ino
@@ -15,7 +15,7 @@
 /////////////////////////////
 // Physical Properties of the robot
 #define WHEEL_BASE           10 //inches // I forgot Y I did this in inches
-#define ENCODER_RESOLUTION   3 // right_pulses per revolution
+#define ENCODER_RESOLUTION   3 // pulses per revolution
 #define WHEEL_DIAMETER       12 // cm
 #define PI                   3.1415926535897932384626433832795
 /////////////////////////////
@@ -29,6 +29,7 @@ float D_GAIN=           0.4;
 #include <ros/time.h>
 
 #include <std_msgs/Int16.h>
+#include <ackermann_msgs/AckermannDriveStamped.h>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
 
@@ -41,22 +42,22 @@ int CheckClamp(int x) {
 }
  void set_Throttle_Goal(int speed) { // -100 :-: 100
   speed = constrain(speed, -MAX_SPEED, MAX_SPEED); // Speed Limit
-  goal_throttle = speed;
+  goal_velocity = speed;
 }
 void UpdatePIDController(){
   // compute the error between the measurement and the desired value
-  throttleError = goal_throttle - actual_throttle;
-  DerivativeTerm = throttleError - lastThrottleError;
+  velocityError = goal_velocity - actual_velocity;
+  DerivativeTerm = velocityError - lastVelocityError;
   
   // If the actuator is saturating ignore the integral term
   // if the system is clamped and the sign of the integrator term and the sign of the PID output are the same
   if (Clamped and ((PID_Output/abs(PID_Output))==(IntegralTerm/abs(IntegralTerm)))){ 
     IntegralTerm += 0;
   } else {
-    IntegralTerm += throttleError;
+    IntegralTerm += velocityError;
   }
   // compute the control effort by multiplying the error by Kp
-  PID_Output = (throttleError * P_GAIN) + (IntegralTerm * I_GAIN) + (DerivativeTerm * D_GAIN);
+  PID_Output = (velocityError * P_GAIN) + (IntegralTerm * I_GAIN) + (DerivativeTerm * D_GAIN);
   current_throttle_setting += PID_Output; 
 
   // make sure the output value is bounded to 0 to 100 using the bound function defined below
@@ -64,32 +65,8 @@ void UpdatePIDController(){
   set_Throttle(current_throttle_setting); // then write it to the LED pin to change control voltage to LED
 }
 
-void Blink(int times){
-  while(times<0){
-    digitalWrite(LED_BUILTIN, HIGH); 
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
-    times-=1; 
-  }
-}
-
 double mapf(double val, double in_min, double in_max, double out_min, double out_max) {
     return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-int parseIntFast(int numberOfDigits){
-  /*
-  This function returns the converted integral number as an int value.
-  If no valid conversion could be performed, it returns zero.*/
-  char theNumberString[numberOfDigits + 1];
-  int theNumber;
-  for (int i = 0; i < numberOfDigits; theNumberString[i++] = Serial.read()){
-    delay(5);
-    };
-  theNumberString[numberOfDigits] = 0x00;
-  theNumber = atoi(theNumberString);
-  return theNumber;
 }
 
 //Globals
@@ -106,37 +83,38 @@ bool Clamped = false;
 int IntegralTerm = 0;
 int DerivativeTerm = 0;
 int PID_Output = 0; 
-int throttleError = 0;
-int lastThrottleError = 0;
+int velocityError = 0;
+int lastVelocityError = 0;
 
 ros::NodeHandle nh;
 
 std_msgs::Int16 ticks_msg;
-std_msgs::Int16 right_ticks_msg;
+ackermann_msgs::AckermannDriveStamped state_msg;
 
 ros::Publisher pub_ticks("/ticks",&ticks_msg);
 ros::Publisher pub_right_ticks("/right_ticks",&right_ticks_msg);
 
-void DriverCallback(const geometry_msgs::Twist&);
-ros::Subscriber<geometry_msgs::Twist> drive("cmd_vel", &DriverCallback); 
+void DriverCallback(const ackermann_msgs::AckermannDriveStamped&);
+ros::Subscriber<ackermann_msgs::AckermannDriveStamped> drive("ackermann_cmd", &DriverCallback); 
 
 void getMotorData(unsigned long time) {
   double delta_time = double(time)/1000; // must be in seconds for these formulas 
   
   rpm = (60*(double(pulses - old_pulses)/ENCODER_RESOLUTION))/delta_time; // 60 is to convert from seconds to minutes
   ticks_msg.data += pulses - old_pulses;
-  old_left_pulses = pulses;
+  old_pulses = pulses;
+  state_msg.drive.speed = 1;
 }
-void EncoderEvent() { // Counts right_pulses on the  Encoder
+void EncoderEvent() { // Counts pulses on the  Encoder
   if (digitalRead(EncoderPin) == LOW) {
     if (moving_forward){
-      right_pulses++;
+      pulses++;
     } else {
-      right_pulses--;
+      pulses--;
     }
   }
 }
- Servo Throttle;
+Servo Throttle;
 Servo Steering;
 void Brake(){ // Disengages the brake on the ESC
   moving_forward = false; // Set first to prevent recurion 
@@ -156,22 +134,20 @@ void set_Throttle(int _speed) { // -100 :-: 100
   _speed = map(_speed,-100,100,55,145);
   Throttle.write(_speed);
 }
-void DriverCallback(const geometry_msgs::Twist& cmd_msg) {
+void DriverCallback(const ackermann_msgs::AckermannDriveStamped& cmd_msg) {
   // Lin -.5:.5  Ang -1.5:1.5
-  
-    set_Throttle(mapf(cmd_msg.linear.x,-.5,.5,-100,100));
-    Steering.write(mapf(cmd_msg.angular.z,-.5,.5,0,180));
+    set_Throttle(mapf(cmd_msg.drive.speed,-.5,.5,-100,100));
+    Steering.write(mapf(cmd_msg.drive.steering_angle,-.5,.5,0,180));
+    state_msg.drive.steering_angle = cmd_msg.drive.steering_angle;
 }
-void PublishTICKS(unsigned long time) {
-  pub_ticks.publish(&ticks_msg);
-  ticks_msg.data = 0;
+void PublishState(unsigned long time) {
+  pub_state.publish(&state_msg);
 }
 void setup() {
   nh.initNode();
   nh.getHardware()->setBaud(57600); // Ros Node uses 57600 by default
   nh.subscribe(drive);
-  nh.advertise(pub_ticks);
-  ticks_msg.data = 0;
+  nh.advertise(pub_state);
   
   pinMode(ThrottleControlPin, OUTPUT); 
   pinMode(EncoderPin, INPUT); 
@@ -196,7 +172,7 @@ void loop() {
   unsigned long time = millis(); // time - lastMilli == time passed
   if(time - lastMilli >= LoopTime)   { // Enter Timed Loop 
     getMotorData(time - lastMilli);
-    PublishTICKS(time - lastMilli);// Publish and Restart Loop 
+    PublishState(time - lastMilli);// Publish and Restart Loop 
     lastMilli = time;
   }
 }
