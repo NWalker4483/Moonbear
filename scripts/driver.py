e0d15f76f66ab377bbd992aaf9847330d459142b
Nile Walker <niwal7@morgan.edu>
2021-02-19 10:43:03 -0500

    Added PID Driver

--- scripts/driver.py
+++ scripts/driver.py
@@ -2,17 +2,24 @@
 import rospy
 from geometry_msgs.msg import Twist
 
+from nav_msgs.msg import Odometry
+
 from std_msgs.msg import Float64
 from ackermann_msgs.msg import AckermannDriveStamped
 import serial
 
+
+def odom_callback(data):
+  global state_pub
+  state_pub.publish(data.linear.x)
+
 def pid_callback(data):
   global ser
-  ser.write(b'T{0}\nS{1}'.format(data, steering_angle))  
+  ser.write(b'T{0}\nS{1}\n'.format(data, steering_angle))  
 
 
 def cmd_callback(data):
-  global pub
+  global set_pub
   global steering_angle
   steering_angle = int(90 + (data.angular.z * (180/3.14)))
   pub.publish(data.linear.x)
@@ -22,11 +29,15 @@ if __name__ == '__main__':
     steering_angle = 0
     rospy.init_node('Linear_PID_Controller')
     twist_cmd_topic = rospy.get_param('~twist_cmd_topic', '/cmd_vel') 
+    odom_topic = rospy.get_param('~odom_topic', "/camera/odom/sample") 
 
-    rospy.Subscriber(twist_cmd_topic, Twist, cmd_callback, queue_size=1)
-
-    pub = rospy.Publisher("/setpoint", Float64, queue_size=1)
+    
+    set_pub = rospy.Publisher("/setpoint", Float64, queue_size=1)
     rospy.Subscriber("/control_effort", Float64, pid_callback, queue_size=1)
+    state_pub = rospy.Publisher("/state", Float64, queue_size=1)
+
+    rospy.Subscriber(odom_topic, Odometry, odom_callback, queue_size=1)
+    rospy.Subscriber(twist_cmd_topic, Twist, cmd_callback, queue_size=1)
 
     ser = serial.Serial('/dev/ttyACM0')
     
