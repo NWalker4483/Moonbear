f963f01b62cd14a89773693be04f9622f82e8854
rc <niwal7@morgan.edu>
2021-02-19 15:08:31 -0500

    New Driver Code

--- scripts/driver.py
+++ scripts/driver.py
@@ -1,21 +1,21 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
 
 from nav_msgs.msg import Odometry
 
 from std_msgs.msg import Float64
-from ackermann_msgs.msg import AckermannDriveStamped
-import serial
+#import serial
 
 
 def odom_callback(data):
   global state_pub
-  state_pub.publish(data.linear.x)
+  rospy.loginfo(data)
+  state_pub.publish(data.twist.twist.linear.x)
 
 def pid_callback(data):
   global ser
-  ser.write(b'T{0}\nS{1}\n'.format(data, steering_angle))  
+  #ser.write(b'T{0}\nS{1}\n'.format(data, steering_angle))  
 
 
 def cmd_callback(data):
@@ -25,13 +25,12 @@ def cmd_callback(data):
   set_pub.publish(data.linear.x)
 
 if __name__ == '__main__': 
-  try:    
+  try:
     steering_angle = 0
     rospy.init_node('Linear_PID_Controller')
     twist_cmd_topic = rospy.get_param('~twist_cmd_topic', '/cmd_vel') 
     odom_topic = rospy.get_param('~odom_topic', "/camera/odom/sample") 
 
-    
     set_pub = rospy.Publisher("/setpoint", Float64, queue_size=1)
     rospy.Subscriber("/control_effort", Float64, pid_callback, queue_size=1)
     state_pub = rospy.Publisher("/state", Float64, queue_size=1)
@@ -39,9 +38,8 @@ if __name__ == '__main__':
     rospy.Subscriber(odom_topic, Odometry, odom_callback, queue_size=1)
     rospy.Subscriber(twist_cmd_topic, Twist, cmd_callback, queue_size=1)
 
-    ser = serial.Serial('/dev/ttyACM0')
-    
+    #ser = serial.Serial('/dev/ttyACM0')
     rospy.spin()
-    
+
   except rospy.ROSInterruptException:
     pass
